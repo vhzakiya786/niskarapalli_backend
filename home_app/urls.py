ceec0cdcd82b33a008f141fca79daa7dd2e83271
unknown <vhzakiya786@gmail.com>
2025-04-22 19:35:28 +0530

    userform

--- home_app/urls.py
+++ home_app/urls.py
@@ -1,13 +1,19 @@
 
 from rest_framework import routers
-
+from . import views
 from home_app.views import TestViewset, UserViewset
-
+from django.urls import path
 router=routers.SimpleRouter()
 
 
 
 router.register(r'user',UserViewset,basename='user')
 router.register(r'name',TestViewset,basename='test')
+urlpatterns=[
+    path('', views.user_create_update, name='user_create_update'),
+    path('get_users/', views.get_users, name='get_users'),
+    path('get_upi_ids/', views.get_upi_ids, name='get_upi_ids'),
+    path('check_user_by_upi/', views.check_user_by_upi, name='check_user_by_upi'),
+]
 
-urlpatterns=router.urls
+urlpatterns+=router.urls
